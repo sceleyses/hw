5a111e2ac31de7745904f05f588d079659bc50d1
sceleyses <potochylova@knu.ua>
2025-03-20 19:30:02 +0200

    first commit

---
+++ task7/Figure.py
@@ -0,0 +1,21 @@
+
+class Figure:
+    def __init__(self):
+
+    def dimention():
+
+    def perimetr():
+
+    def square():
+
+    def squareSurface():
+
+    def squareBase():
+
+    def height():
+
+    def volume():
+
+    def __str__(self):
+
+
