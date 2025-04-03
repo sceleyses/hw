5407810c3705c86f64e3d3004d7a13d68887cbbb
sceleyses <potochylova@knu.ua>
2025-04-03 12:19:18 +0300

    add check

--- task 9-10/Rational.py
+++ task 9-10/Rational.py
@@ -1,16 +1,38 @@
 class Rational:
-    def __init__(self,numerator, denumerator=1 ):
-        self.n = numerator
-        self.d = denumerator
+    def __init__(self,numerator, denominator=1 ):
+        if isinstance(numerator, str):
+            parts = numerator.split('/')
+            if len(parts) == 2:
+                self.n = int(parts[0])
+                self.d = int(parts[1])
+            else:
+                raise ValueError("Неприпустимий формат!")
+
+            if denominator != 1:
+                raise ValueError("Неприпустиме значення!")
+        else:
+            if not isinstance(numerator, int) or not isinstance(denominator, int):
+                raise TypeError("Чисельник та знаменник повинні бути цілими числами!")
+            self.n = numerator
+            self.d = denominator
+
+        if self.d == 0:
+            raise ValueError("На нуль ділити неможна!")
 
     def __add__(self, other):
         new_n = self.n * other.d + other.n * self.d
-        new_d = self.d * other.d
+        if self.d != other.d:
+            new_d = self.d * other.d
+        else:
+            new_d = self.d
         return Rational(new_n, new_d)
 
     def __sub__(self, other):
         new_n = self.n * other.d - other.n * self.d
-        new_d = self.d * other.d
+        if self.d != other.d:
+            new_d = self.d * other.d
+        else:
+            new_d = self.d
         return Rational(new_n, new_d)
 
     def __mul__(self, other):
@@ -24,5 +46,17 @@ class Rational:
 
 if __name__ == "__main__":
     r1 = Rational(5)
-    r2 = Rational(3)
-    print(r1 * r2)
+    r2 = Rational(5, 6)
+    print(r1 + r2)
+
+    r3 = Rational("5/3")
+    print(r3 + r3)
+
+    r4 = Rational("10/2")
+    print(r4 * r1)
+
+    # r5 = Rational(3, 0)
+    # print(r5)
+
+    # r6 = Rational("4/2", 5)
+    # print(r6)
