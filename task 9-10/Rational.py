a72157f137db1be676d697131ce48f713109cbf2
sceleyses <potochylova@knu.ua>
2025-04-03 11:28:40 +0300

    sketch commit

---
+++ task 9-10/Rational.py
@@ -0,0 +1,15 @@
+class Rational:
+    def __init__(self,numerator, denumerator=1 ):
+        self.n = numerator
+        self.d = denumerator
+
+    def __add__(self, other): # +
+
+    def __sub__(self, other): # -
+
+    def __mul__(self, other): # *
+
+
+    def __str__(self):
+            return f"{self.n}/{self.d}"
+
