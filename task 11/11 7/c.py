8b84f07ea26c40d7cf9468cab50e2402316e8b31
sceleyses <potochylova@knu.ua>
2025-04-10 22:24:18 +0300

    task 11 update

--- task 11/11 7/c.py
+++ task 11/11 7/c.py
@@ -1,6 +1,7 @@
 import math
+
 xk = 1
-x = int(input())
+x = int(input("x= "))
 for k in range(1, 11):
     xk = xk * (math.factorial(x*(k*2-k)))/math.factorial(k**2 + k)
     print(xk)
--- task 11/11 7/d.py
+++ task 11/11 7/d.py
@@ -1,5 +1,5 @@
 xk = 1
-x = int(input())
+x = int(input("x= "))
 for k in range(1, 11):
     xk = xk * ((x * k + x)/ k**2)
     print(xk)
