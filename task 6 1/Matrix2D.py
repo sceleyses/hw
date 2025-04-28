9052d06ea6b278942c75961eb99fbdb978d71980
sceleyses <potochylova@knu.ua>
2025-04-28 17:55:25 +0300

    updated Matrix

---
+++ task 6 1/Matrix2D.py
@@ -0,0 +1,83 @@
+from Vector2D import Vector2D
+
+class Matrix2D:
+    def __init__(self, *a):
+        if len(a) == 1:
+            if isinstance(a[0], Matrix2D):
+                self.a11, self.a12, self.a21, self.a22 = a[0].a11, a[0].a12, a[0].a21, a[0].a22
+            elif len(a[0]) == 4:
+                self.a11, self.a12, self.a21, self.a22 = map(float, a[0])
+            else:
+                if len(a[0]) != 2 or len(a[0][0]) != 2 or len(a[0][1]) != 2:
+                    raise ValueError("Неправильна структура матриці")
+
+                self.a11, self.a12 = map(float, a[0][0])
+                self.a21, self.a22 = map(float, a[0][1])
+
+        elif len(a) == 4:  # вхідний параметр - 4 числа, перші два - перший рядок, 3, 4 - другий рядок.
+
+            # self.a11 = a[0]
+            # self.a12 = a[1]
+            # self.a21 = a[2]
+            # self.a22 = a[3]
+
+            self.a11, self.a12, self.a21, self.a22 = map(float, a)
+
+        elif len(a) == 0:  # одинична матриця
+            self.a11, self.a12, self.a21, self.a22 = 1.0, 0.0, 0.0, 1.0
+
+        elif len(a) == 2: # приходить два вектори Vector2D
+            if isinstance(a[0], Vector2D) and isinstance(a[1], Vector2D):
+                self.a11, self.a21 = a[0].v1, a[0].v2
+                self.a12, self.a22 = a[1].v1, a[1].v2
+        else:
+            raise ValueError("По таких даних неможливо створити матрицю!")
+
+    def __str__(self):
+        return f"{self.a11:.2f} {self.a12:.2f}\n{self.a21:.2f} {self.a22:.2f}"
+
+    def col(self, num):
+        if num == 1:
+            return Vector2D(self.a11, self.a21)
+        elif num == 2:
+            return Vector2D(self.a12, self.a22)
+        else:
+            raise IndexError("Неправильний номер стовпчика")
+
+    def det(self):
+        return self.a11 * self.a22 - self.a12 * self.a21
+
+    def is_degenerate(self):
+        return abs(self.det()) < 0.000001
+
+if __name__ == '__main__':
+
+    v1 = Vector2D(1, 2)
+    v2 = Vector2D(3, 4)
+    # m = Matrix2D(v1, v2)
+    # print(m)
+    #
+    # b1 = m.col(2)
+    # print(b1)
+
+
+    # m = Matrix2D(1, 2, 3, 4)
+    # print(m)
+
+    # m1 = Matrix2D()
+    # print(m1)
+    # print(m1.det())
+    # print(m1.is_degenerate())
+
+    # m1 = Matrix2D(m)
+    # m2 = Matrix2D(
+    #     [
+    #         [1, 2],
+    #         [3, 4]
+    #     ]
+    # )
+    # m3 = Matrix2D()
+    # m4 = Matrix2D(1, 2, 3)  #  має бути породжена помилка
+
+    # s1 = int("5")
+    # s2 = int("5_hello")
